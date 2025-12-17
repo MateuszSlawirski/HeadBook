dce351676cf3c67c52a5b3be12131b93c588447b
MateuszSlawirski <m.slawirski@hotmail.de>
2025-12-17 20:30:51 +0100

    Config Upload für Frontend

---
+++ config.js
@@ -0,0 +1,9 @@
+// --- 1. FIREBASE KONFIGURATION (Hier deine Daten einfügen!) ---
+export const firebaseConfig = {
+  apiKey: "AIzaSyB1T397DUasIVEQXKNELNAX_2itPWikMII",
+  authDomain: "riderpoint.firebaseapp.com",
+  projectId: "riderpoint",
+  storageBucket: "riderpoint.firebasestorage.app",
+  messagingSenderId: "426779644316",
+  appId: "1:426779644316:web:41f6f90d041ee6ff037859"
+};
